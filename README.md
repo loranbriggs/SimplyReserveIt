1651f5e6f0fcb1b3bcf3dea0d7e530fbde6de055
Loran Briggs <loranbriggs@gmail.com>
2012-10-31 16:15:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simply Reserve It
+
+Simply yet elegant way for your customers to reserve there place in line, table, you name it.<br>
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplyReserveIt::Application.config.secret_token = '77d1c06e8df88e0344684b7a1c23ddd88eb06c959944d64a247ee55f1640f14d98ffcb0a098d81a9dffc69d4e8d798c6758917cf23c9e3557189a3f713430916'
